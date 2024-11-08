d26a6aa7ff422e2a4acefcb7dc0098b382e5e30e
hunghg255 <giahunghust@gmail.com>
2024-11-08 14:25:48 +0700

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Agiletech test')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      displayOperationId: true,
+    },
+  });
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
