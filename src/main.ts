dc2d4d64521a77429a9efb7a315c405663c6c6d9
hunghg255 <giahunghust@gmail.com>
2024-11-08 14:33:36 +0700

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-  });
+	app.enableCors({
+		origin: "*"
+	});
 
-  const config = new DocumentBuilder()
-    .setTitle('Agiletech test')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+	const config = new DocumentBuilder()
+		.setTitle("Agiletech test")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
 
-  const document = SwaggerModule.createDocument(app, config);
+	const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document, {
-    swaggerOptions: {
-      displayOperationId: true,
-    },
-  });
-  const PORT = process.env.PORT || 3000;
-  await app.listen(PORT);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+	SwaggerModule.setup("backend", app, document, {
+		swaggerOptions: {
+			displayOperationId: true
+		}
+	});
+	const PORT = process.env.PORT || 3000;
+	await app.listen(PORT);
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
